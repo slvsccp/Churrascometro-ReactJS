3ebaf40b9f170d55818ade5b5c5d321712a8e1f4
Matheus Silva <teus_26@outlook.com>
2024-08-28 17:52:04 -0300

    validações

--- src/components/CalculadoraChurrasco.tsx
+++ src/components/CalculadoraChurrasco.tsx
@@ -3,35 +3,55 @@ import { nomesAlimentos } from "../types";
 import { Formik, Field, Form } from "formik";
 import * as Yup from "yup";
 
-const CalculadoraChurrasco = () => {
-    const navigate = useNavigate();
+const esquemaValidacao = Yup.object().shape({
+    pessoas: Yup.number().min(1, "Número de pessoas é obrigatório."),
+    selecaoAlimentos: Yup.array()
+    .of(Yup.string())
+    .test(
+      "check-selecaoAlimentos",
+      "Selecione pelo menos um alimento.",
+      (array) => array !== null && array!.length > 0
+    ),
+});
 
-    return (
-        <div>
-            <Formik 
-                initialValues={{ pessoas: 0, selecaoAlimentos: [] }}
-                onSubmit={() => {
-                    navigate("/resultado")
-                    console.log("teste")
-                }}
-            >
-                <Form>
-                    <div>
-                        <label htmlFor="pessoas">Número de pessoas:</label>
-                        <Field name="pessoas" type="number" />
-                    </div>
-                    <h2>Selecione os alimentos para o Churrasco:</h2>
-                    {Object.keys(nomesAlimentos).map((alimento) => (
-                        <div>
-                            <Field type="checkbox" name="selecaoAlimentos" value={alimento} />
-                            <label htmlFor="selecaoAlimentos">{nomesAlimentos[alimento]}</label>
-                        </div>
-                    ))}
-                    <button type="submit">Calcular</button>
-                </Form>
-            </Formik>
-        </div>
-    )
+const CalculadoraChurrasco = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <Formik
+        initialValues={{ pessoas: 0, selecaoAlimentos: [] }}
+        validationSchema={esquemaValidacao}
+        onSubmit={(values) => {
+          navigate("/resultado", {
+            state: {
+              pessoas: values.pessoas,
+              alimentosSelecionados: values.selecaoAlimentos,
+            }
+          })
+        }}
+      >
+        {({ errors, touched }) => (
+          <Form>
+            <div>
+              <label htmlFor="pessoas">Número de pessoas:</label>
+              <Field name="pessoas" type="number" />
+              {errors.pessoas && touched.pessoas ? (<p>{errors.pessoas}</p>) : null}
+            </div>
+            <h2>Selecione os alimentos para o Churrasco:</h2>
+            {Object.keys(nomesAlimentos).map((alimento) => (
+              <div>
+                <Field type="checkbox" name="selecaoAlimentos" value={alimento} />
+                <label htmlFor="selecaoAlimentos">{nomesAlimentos[alimento]}</label>
+              </div>
+            ))}
+            {errors.selecaoAlimentos && touched.selecaoAlimentos ? (<p>{errors.selecaoAlimentos}</p>) : null}
+            <button type="submit">Calcular</button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  )
 };
 
 export default CalculadoraChurrasco;
